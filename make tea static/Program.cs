3da4a755139c8df65d4c8e9cd2d4fb833037a4c6
Sajjad5591 <sajjadirfan5591@gmail.com>
2025-04-20 22:19:17 -0700

    Add project files.

---
+++ make tea static/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace consoleApplication3
+{
+    public class Program
+    {
+      static void maketea()
+        {
+            Console.WriteLine("Making tea...");
+            Console.WriteLine("Boiling water...");
+            Console.WriteLine("Steeping tea...");
+            Console.WriteLine("Pouring tea...");
+            Console.WriteLine("Adding milk and sugar...");
+            Console.WriteLine("Tea is ready!");
+        }
+
+        static void Main(string[] args)
+        {
+           maketea();
+           // Console.WriteLine("Press any key to exit...");
+            //Console.ReadKey();
+        }
+    }
+
+}
+
